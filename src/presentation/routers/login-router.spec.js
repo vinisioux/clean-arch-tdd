b2164feb1171705e376a9850097ce55b5755437e
vinisioux <vile.pb@gmail.com>
2022-04-19 14:50:37 -0300

    router returns 400 if password isnt provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400,
       };
@@ -14,6 +14,21 @@ describe("Login Router", () => {
 
     const httpRequest = {
       body: {
+        password: "123123",
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test("Should return 400 if password is not provided", () => {
+    const sut = new LoginRouter();
+
+    const httpRequest = {
+      body: {
+        email: "test@test.com",
         password: "123123",
       },
     };
