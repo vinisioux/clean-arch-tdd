d3b9e407709e5fab001899e9c43f88a56b73a4af
vinisioux <vile.pb@gmail.com>
2022-04-19 17:10:39 -0300

    test login router return 200 when valid params are valid

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -24,9 +24,13 @@ class LoginRouter {
       return HttpResponse.badRequest("password");
     }
 
-    this.authUseCase.auth(email, password);
+    const accessToken = this.authUseCase.auth(email, password);
 
-    return HttpResponse.unauthorizedError();
+    if (!accessToken) {
+      return HttpResponse.unauthorizedError();
+    }
+
+    return HttpResponse.ok();
   }
 }
 
