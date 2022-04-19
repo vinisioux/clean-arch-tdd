1355ff2b3f96354d63c1aafa307973bd5bb53542
vinisioux <vile.pb@gmail.com>
2022-04-19 14:30:05 -0300

    test for login router returns 400 if emails isnt provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,25 @@
+class LoginRouter {
+  route(httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+      };
+    }
+  }
+}
+
+describe("Login Router", () => {
+  test("Should return 400 if email is not provided", () => {
+    const sut = new LoginRouter();
+
+    const httpRequest = {
+      body: {
+        password: "123123",
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
