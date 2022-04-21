158c09f4901fe9c07905929b52d4079b41f830da
vinisioux <vile.pb@gmail.com>
2022-04-21 15:41:11 -0300

    refactor authUsecase

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -25,20 +25,16 @@ class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email);
+    const isValid =
+      user && (await this.encrypter.compare(password, user.password));
 
-    if (!user) {
-      return null;
+    if (isValid) {
+      const acecssToken = await this.tokenGenerator.generate(user.id);
+
+      return acecssToken;
     }
 
-    const isValid = await this.encrypter.compare(password, user.password);
-
-    if (!isValid) {
-      return null;
-    }
-
-    const acecssToken = await this.tokenGenerator.generate(user.id);
-
-    return acecssToken;
+    return null;
   }
 }
 
