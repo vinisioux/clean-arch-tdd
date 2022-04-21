3e5ac34cb317afb3bd7fab3e5585eec0e3bfa318
vinisioux <vile.pb@gmail.com>
2022-04-21 13:54:19 -0300

    ensure email validator return false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,15 +1,26 @@
+const validator = require("validator");
+
 class EmailValidator {
   isValid(email) {
-    return true;
+    return validator.isEmail(email);
   }
 }
 
 describe("Email Validator", () => {
-  test("shoud return true if validator returns true", () => {
+  test("should return true if validator returns true", () => {
     const sut = new EmailValidator();
 
     const isEmailValid = sut.isValid("valid_email@mail.com");
 
     expect(isEmailValid).toBe(true);
+  });
+
+  test("should return false if validator returns false", () => {
+    validator.isEmailValid = false;
+    const sut = new EmailValidator();
+
+    const isEmailValid = sut.isValid("invalid_email@mail.com");
+
+    expect(isEmailValid).toBe(false);
   });
 });
