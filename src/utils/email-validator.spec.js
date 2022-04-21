437a5ad8a6a24cc93e44c907205c128e30504eba
vinisioux <vile.pb@gmail.com>
2022-04-21 13:55:00 -0300

    move sut creation to a factory

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,9 +6,13 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator();
+};
+
 describe("Email Validator", () => {
   test("should return true if validator returns true", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
 
     const isEmailValid = sut.isValid("valid_email@mail.com");
 
@@ -17,7 +21,7 @@ describe("Email Validator", () => {
 
   test("should return false if validator returns false", () => {
     validator.isEmailValid = false;
-    const sut = new EmailValidator();
+    const sut = makeSut();
 
     const isEmailValid = sut.isValid("invalid_email@mail.com");
 
