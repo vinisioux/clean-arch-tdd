37011c89bb8b12248072804ec7626c7346ae7689
vinisioux <vile.pb@gmail.com>
2022-04-21 15:38:40 -0300

    ensure AuthUseCase return an accessToken if valid credentials

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -36,7 +36,9 @@ class AuthUseCase {
       return null;
     }
 
-    await this.tokenGenerator.generate(user.id);
+    const acecssToken = await this.tokenGenerator.generate(user.id);
+
+    return acecssToken;
   }
 }
 
