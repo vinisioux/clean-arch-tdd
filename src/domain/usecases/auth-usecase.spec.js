ce16ab38d9c5d754b3f48e1f5b571c35cf657e01
vinisioux <vile.pb@gmail.com>
2022-04-21 14:22:18 -0300

    ensure AuthUseCase throws missing param error if no email is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,9 @@
+const { MissingParamError } = require("../../utils/errors");
+
 class AuthUseCase {
   async auth(email) {
     if (!email) {
-      throw new Error();
+      throw new MissingParamError("email");
     }
   }
 }
@@ -12,6 +14,6 @@ describe("Auth UseCase", () => {
 
     expect(async () => {
       await sut.auth();
-    }).rejects.toThrow();
+    }).rejects.toThrow(new MissingParamError("email"));
   });
 });
