c71b2891653a5edce3b42541d186747b981fe7a5
vinisioux <vile.pb@gmail.com>
2022-04-23 18:55:22 -0300

    ensure token generator returns a token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,15 +1,24 @@
+const jwt = require("jsonwebtoken");
 class TokenGenerator {
   async generate(id) {
-    return null;
+    return jwt.sign(id, "secret");
   }
 }
 
 describe("Token Generator", () => {
   test("Should return null if jwt lib returns null", async () => {
     const sut = new TokenGenerator();
-
+    jwt.token = null;
     const token = await sut.generate("any_id");
 
     expect(token).toBe(null);
+  });
+
+  test("Should return a token if jwt lib returns token", async () => {
+    const sut = new TokenGenerator();
+
+    const token = await sut.generate("any_id");
+
+    expect(token).toBe(jwt.token);
   });
 });
