253f6ea1299c2b9d3dfb75536fffdf4df9d4bf36
vinisioux <vile.pb@gmail.com>
2022-04-23 20:05:45 -0300

    move sut creation to a factory method

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,6 +1,7 @@
 require("dotenv/config");
 const { MongoClient } = require("mongodb");
 
+let client, db;
 class LoadUserByEmailRepository {
   constructor(userModel) {
     this.userModel = userModel;
@@ -12,9 +13,17 @@ class LoadUserByEmailRepository {
   }
 }
 
-describe("LoadUserByEmail Repository", () => {
-  let client, db;
+const makeSut = () => {
+  const userModel = db.collection("users");
+  const sut = new LoadUserByEmailRepository(userModel);
 
+  return {
+    sut,
+    userModel,
+  };
+};
+
+describe("LoadUserByEmail Repository", () => {
   beforeAll(async () => {
     client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -33,22 +42,18 @@ describe("LoadUserByEmail Repository", () => {
   });
 
   test("Should return null if user not found", async () => {
-    const userModel = db.collection("users");
-
-    const sut = new LoadUserByEmailRepository(userModel);
+    const { sut } = makeSut();
     const user = await sut.load("invalid_email@email.com");
 
     expect(user).toBe(null);
   });
 
   test("Should return an user if user is found", async () => {
-    const userModel = db.collection("users");
+    const { sut, userModel } = makeSut();
 
     await userModel.insertOne({
       email: "valid_email@email.com",
     });
-
-    const sut = new LoadUserByEmailRepository(userModel);
 
     const user = await sut.load("valid_email@email.com");
 
