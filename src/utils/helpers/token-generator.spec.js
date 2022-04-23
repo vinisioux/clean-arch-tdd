c8a8f7ac7ab396dac6935dc65d6350bc89bb7a78
vinisioux <vile.pb@gmail.com>
2022-04-23 19:09:27 -0300

    move tokenGenerator class to a file

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,22 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { MissingParamError } = require("../errors/missing-param-error");
-class TokenGenerator {
-  constructor(secret) {
-    this.secret = secret;
-  }
-
-  async generate(id) {
-    if (!this.secret) {
-      throw new MissingParamError("secret");
-    }
-
-    if (!id) {
-      throw new MissingParamError("id");
-    }
-
-    return jwt.sign(id, this.secret);
-  }
-}
+const { TokenGenerator } = require("./token-generator");
 
 const makeSut = () => {
   const sut = new TokenGenerator("secret");
