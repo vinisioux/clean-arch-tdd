9a573932d7d2303bf63930cac98feaa283f9f3fe
vinisioux <vile.pb@gmail.com>
2022-04-23 20:01:54 -0300

    ensure loadUserByEmailRepository returns an user if found

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   collectCoverageFrom: ["**/src/**/*.js"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  preset: "@shelf/jest-mongodb",
 };
--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,14 +1,57 @@
+require("dotenv/config");
+const { MongoClient } = require("mongodb");
+
 class LoadUserByEmailRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
   async load(email) {
-    return null;
+    const user = this.userModel.findOne({ email });
+
+    return user;
   }
 }
 
 describe("LoadUserByEmail Repository", () => {
+  let client, db;
+
+  beforeAll(async () => {
+    client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    db = await client.db();
+  });
+
+  beforeEach(async () => {
+    await db.collection("users").deleteMany();
+  });
+
+  afterAll(async () => {
+    await client.close();
+  });
+
   test("Should return null if user not found", async () => {
-    const sut = new LoadUserByEmailRepository();
+    const userModel = db.collection("users");
+
+    const sut = new LoadUserByEmailRepository(userModel);
     const user = await sut.load("invalid_email@email.com");
 
     expect(user).toBe(null);
+  });
+
+  test("Should return an user if user is found", async () => {
+    const userModel = db.collection("users");
+
+    await userModel.insertOne({
+      email: "valid_email@email.com",
+    });
+
+    const sut = new LoadUserByEmailRepository(userModel);
+
+    const user = await sut.load("valid_email@email.com");
+
+    expect(user.email).toBe("valid_email@email.com");
   });
 });
