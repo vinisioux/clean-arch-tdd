9bedf655c2b0d2cf1977c35f8c6fed8b263744bc
vinisioux <vile.pb@gmail.com>
2022-04-24 20:01:57 -0300

    enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -2,13 +2,25 @@ const request = require("supertest");
 const { app } = require("./app");
 
 describe("App Setup", () => {
-  test("Should diseble x-powered-by", async () => {
+  test("Should disable x-powered-by", async () => {
     app.get("/test_x_powered_by", (req, res) => {
       return res.send("");
     });
 
-    const response = await request(app).get("/");
+    const response = await request(app).get("/test_x_powered_by");
 
     expect(response.headers["x-powered-by"]).toBeUndefined();
+  });
+
+  test("Should enable cores", async () => {
+    app.get("/test_cors", (req, res) => {
+      return res.send("");
+    });
+
+    const response = await request(app).get("/test_cors");
+
+    expect(response.headers["access-control-allow-origin"]).toBe("*");
+    expect(response.headers["access-control-allow-methods"]).toBe("*");
+    expect(response.headers["access-control-allow-headers"]).toBe("*");
   });
 });
