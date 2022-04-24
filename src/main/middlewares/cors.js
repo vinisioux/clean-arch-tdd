4af8531179bc3fad0b2064d07dd7d9fc9cec7aae
vinisioux <vile.pb@gmail.com>
2022-04-24 20:04:04 -0300

    move cors to a fle

---
+++ src/main/middlewares/cors.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cors: (req, res, next) => {
+    res.set("access-control-allow-origin", "*");
+    res.set("access-control-allow-methods", "*");
+    res.set("access-control-allow-headers", "*");
+    next();
+  },
+};
