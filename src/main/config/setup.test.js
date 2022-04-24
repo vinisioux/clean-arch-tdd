4992f0796553b18f6ab091b15b5bb7f0e73ad47d
vinisioux <vile.pb@gmail.com>
2022-04-24 19:58:29 -0300

    disable x-powered-by header

---
+++ src/main/config/setup.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const { app } = require("./app");
+
+describe("App Setup", () => {
+  test("Should diseble x-powered-by", async () => {
+    app.get("/test_x_powered_by", (req, res) => {
+      return res.send("");
+    });
+
+    const response = await request(app).get("/");
+
+    expect(response.headers["x-powered-by"]).toBeUndefined();
+  });
+});
