f746b77da0e88c9cc88160b4982c631ed5bc0886
vinisioux <vile.pb@gmail.com>
2022-04-25 00:23:19 -0300

    mongodb connection

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,17 @@
-const { app } = require("./config/app");
+require("dotenv/config");
+const MongoHelper = require("../infra/helpers/mongo-helper");
+const { mongoUrl } = require("./config/env");
 
-app.listen(3333, () => {
-  console.log("Server running on port 3333");
-});
+MongoHelper.connect(mongoUrl)
+  .then(() => {
+    console.log("MongoDB Started");
+
+    const { app } = require("./config/app");
+
+    app.listen(3333, () => {
+      console.log("Server running on port 3333");
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
