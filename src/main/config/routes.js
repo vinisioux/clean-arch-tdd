70705f632cd39e914df550e4d9d71c6a2ca3b1b7
vinisioux <vile.pb@gmail.com>
2022-04-25 00:33:47 -0300

    add routes file

---
+++ src/main/config/routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const fastGlob = require("fast-glob");
+
+module.exports = (app) => {
+  app.use("/api", router);
+  fastGlob.sync("**/src/main/routes/**.js").forEach((file) => {
+    require(`../../../${file}`)(router);
+  });
+};
