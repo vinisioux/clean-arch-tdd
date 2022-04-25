29568862a41e9460ff794288d9949664b7360a5e
vinisioux <vile.pb@gmail.com>
2022-04-25 00:08:00 -0300

    ignore test on main layer

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ["**/src/**/*.js"],
+  collectCoverageFrom: ["**/src/**/*.js", "!**/src/main/**"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   preset: "@shelf/jest-mongodb",
