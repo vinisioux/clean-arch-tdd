7ceed6ab2c7379182256b52c4ff911c2d739fe26
vinisioux <vile.pb@gmail.com>
2022-04-25 23:50:20 -0300

    add express router adapter

---
+++ src/main/adapters/express-router-adapter.js
@@ -0,0 +1,17 @@
+class ExpressRouterAdapter {
+  static adapt(router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body,
+      };
+
+      const httpResponse = await router.route(httpRequest);
+
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    };
+  }
+}
+
+module.exports = {
+  ExpressRouterAdapter,
+};
